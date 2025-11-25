e1339d4e0e9314b286171e5a0bed9e7d9ccd7f4f
Tomas Contreras <to.contrerasm@duocuc.cl>
2025-11-25 18:51:10 -0300

    Primer Commit

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+const { animate, scroll, inView } = Motion
+
+// Animación del botón de scroll
+animate('.button-scroll', { 
+  y: [0, -15, 0],
+}, {
+  duration: 1.5,
+  repeat: Infinity,
+  easing: 'ease-in-out'
+})
+
+// Animación de las columnas al hacer scroll usando inView
+const columns = document.querySelectorAll('[class*="col-"]')
+
+columns.forEach((col, index) => {
+  inView(col, () => {
+    animate(col, {
+      opacity: [0, 1],
+      y: [50, 0]
+    }, {
+      duration: 0.8,
+      delay: index * 0.1,
+      easing: 'ease-out'
+    })
+  }, {
+    amount: 0.3
+  })
+})
